477211570b2a6de1c0702ba8dc6a4867d23dd3e4
Eliot Arntz <arntzel@gmail.com>
2013-06-19 16:55:12 -0400

    update

---
+++ TaskListProject/QCCustomTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  QCCustomTableViewCell.h
+//  TaskListProject
+//
+//  Created by Eliot Arntz on 6/19/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCCustomTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *photo;
+
+
+@end
